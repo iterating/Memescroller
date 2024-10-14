3b8b1803e370bac78cf2907b806bd47728135e30
Jonathan Young <jy>
2024-10-13 23:53:44 -0700

    commit

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
+var cors = require('cors')
 
-app.get('/api/search', (req, res) => {
+cors({ origin: 'http://localhost:3000' })
+
+
+app.get('/api', function (req, res, next) {
     const url = 'https://trace.moe/api/search';
     axios.get(url)
     .then(response => {
@@ -14,7 +18,19 @@ app.get('/api/search', (req, res) => {
         }
     })
     .catch(error => {
-        console.log(error);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
         res.status(500).send('Failed to retrieve data');
     });
 });
@@ -22,3 +38,4 @@ app.get('/api/search', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
